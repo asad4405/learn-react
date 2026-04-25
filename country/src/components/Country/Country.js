1ab2df7b09740c0d431ff118411149c148fc2207
Asad <asadrifat450@gmail.com>
2026-04-25 21:23:17 +0600

    country list

--- country/src/components/Country/Country.js
+++ country/src/components/Country/Country.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Country = (props) => {
-    return (
-        <div>
-            
-        </div>
-    );
+  console.log(props.country);
+  const { common,official } = props.country.name;
+  return (
+    <div style={{ border: "1px solid #ccc",margin:'10px',padding:'10px' }}>
+      <h4>Country Name: {common}</h4>
+      <p>Capital:{props.country.capital}</p>
+      <p>Offical:{official}</p>
+      <button>Add Country</button>
+    </div>
+  );
 };
 
 export default Country;
