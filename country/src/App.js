ed4e2ddf4b937bcd128303fa9bbeb416276ef9a4
Asad <asadrifat450@gmail.com>
2026-04-25 21:35:07 +0600

    handle add cart use onclick function

--- country/src/App.js
+++ country/src/App.js
@@ -3,21 +3,33 @@ import "./App.css";
 import Country from "./components/Country/Country";
 
 function App() {
-  const [countries , setCountries] = useState([]);
+  const [countries, setCountries] = useState([]);
 
-  useEffect(()=> {
-    fetch('https://restcountries.com/v3.1/all?fields=name,capital,currencies')
-    .then((res) => res.json())
-    .then(data => setCountries(data))
-  },[])
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all?fields=name,capital,currencies")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+
+  const [cart, setCart] = useState([]);
+  const handleAddCountry = (country) => {
+    const newCart = [...cart, country];
+    setCart(newCart);
+  };
 
   return (
     <div className="App">
       <h3>Total Country: {countries.length}</h3>
-      {countries.map(country => <Country country={country} key={country.name.common}></Country>)}
+      <h3>Cart Added Country: {cart.length}</h3>
+      {countries.map((country) => (
+        <Country
+          country={country}
+          key={country.name.common}
+          handleAddCountry={handleAddCountry}
+        ></Country>
+      ))}
     </div>
   );
 }
-
 
 export default App;
--- country/src/components/Country/Country.js
+++ country/src/components/Country/Country.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 const Country = (props) => {
-  console.log(props.country);
   const { common,official } = props.country.name;
+  const handleAddCountry = props.handleAddCountry;
   return (
     <div style={{ border: "1px solid #ccc",margin:'10px',padding:'10px' }}>
       <h4>Country Name: {common}</h4>
       <p>Capital:{props.country.capital}</p>
       <p>Offical:{official}</p>
-      <button>Add Country</button>
+      <button onClick={() => handleAddCountry(props.country)}>Add Country</button>
     </div>
   );
 };
