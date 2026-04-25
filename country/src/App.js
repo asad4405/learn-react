7bc8e77ee3414eaa1cd4c7e882692aced88f0237
Asad <asadrifat450@gmail.com>
2026-04-25 21:11:10 +0600

    fetch api countries and create Country components

--- country/src/App.js
+++ country/src/App.js
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Country from "./components/Country/Country";
 
 function App() {
+  const [countries , setCountries] = useState([]);
+
+  useEffect(()=> {
+    fetch('https://restcountries.com/v3.1/all?fields=name,capital,currencies')
+    .then((res) => res.json())
+    .then(data => setCountries(data))
+  },[])
+
   return (
     <div className="App">
-      
+      <h3>Total Country: {countries.length}</h3>
+      {countries.map(country => <Country country={country} key={country.name.common}></Country>)}
     </div>
   );
 }
