903be37a788129b22884978e6a4b6ccf4f067555
Asad <asadrifat450@gmail.com>
2026-05-12 22:53:09 +0600

    add homepage category wise product dynamic and homebanner another components

---
+++ ecommerce/src/components/Home/HomeBanner.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { Carousel } from "react-bootstrap";
+const HomeBanner = () => {
+  const apiUrl = process.env.REACT_APP_API_URL;
+  const imageUrl = process.env.REACT_APP_IMAGE_URL;
+  const [mainSliders, setMainsliders] = useState([]);
+  useEffect(() => {
+    fetch(apiUrl + "/mainslider")
+      .then((res) => res.json())
+      .then((data) => setMainsliders(data.data));
+  }, []);
+  return (
+    <div>
+      {/* ============== Banner Section ============= */}
+      <Carousel fade interval={3000}>
+        {mainSliders.map((slider) => (
+          <Carousel.Item key={slider.id}>
+            <a href={slider.link} style={{ textDecoration: "none" }}>
+              <img
+                className="d-block w-100"
+                src={imageUrl + "/" + slider.image}
+                alt="banner1"
+                style={{ height: "450px", objectFit: "cover" }}
+              />
+            </a>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+
+export default HomeBanner;
--- ecommerce/src/components/Home/Homepage.jsx
+++ ecommerce/src/components/Home/Homepage.jsx
@@ -1,33 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Carousel } from "react-bootstrap";
+import HomeBanner from "./HomeBanner";
+import ProductCard from "./ProductCard";
 
 const Homepage = () => {
-  const apiUrl = process.env.REACT_APP_API_URL;
-  const imageUrl = process.env.REACT_APP_IMAGE_URL;
-  const [mainSliders, setMainsliders] = useState([]);
-  useEffect(() => {
-    fetch(apiUrl + "/mainslider")
-      .then((res) => res.json())
-      .then((data) => setMainsliders(data.data));
-  }, []);
+  
 
   return (
     <div>
-      {/* ============== Banner Section ============= */}
-      <Carousel fade interval={3000}>
-        {mainSliders.map((slider) => (
-          <Carousel.Item key={slider.id}>
-            <a href={slider.link} style={{ textDecoration: "none" }}>
-              <img
-                className="d-block w-100"
-                src={imageUrl +"/"+ slider.image}
-                alt="banner1"
-                style={{ height: "450px", objectFit: "cover" }}
-              />
-            </a>
-          </Carousel.Item>
-        ))}
-      </Carousel>
+      <HomeBanner></HomeBanner>
+      <ProductCard></ProductCard>
     </div>
   );
 };
